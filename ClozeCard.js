6487be872a793840f713ef401e8ccc3088786d73
jpalacpac <jpalacpac07@gmail.com>
2017-07-19 16:10:30 -0700

    Fixed bugs

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,6 @@
 var ClozeCard = function(text, cloze) {
   if (this instanceof ClozeCard) {
     this.cloze = cloze;
-
-    if (!text.includes(cloze)) {
-      console.log("Error: " + cloze + " does not exist in text");
-      return;
-    }
     this.partial = text.replace(cloze, "...");
     this.fullText = text;
   }
